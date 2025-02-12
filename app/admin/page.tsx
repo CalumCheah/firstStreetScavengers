ef972b3157df3d2eda6f425570252b484eaa86f2
Michael Nolasco <michael.nolasco@stu.austinisd.org>
2025-02-12 11:07:41 -0600

    jit

---
+++ app/admin/page.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+const AdminDashboard = () => {
+
+    return (
+        <main className={styles.main}>
+            <div className={styles.topbar}>topbar</div>
+            <div className={styles.button}>
+                admin
+            </div>
+            <div className={styles.button}>
+                admin
+            </div>
+        </main>
+    )
+}
+
+export default AdminDashboard
