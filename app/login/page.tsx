95ba42fdf13113e45a1a0eb98ed5e2fad6c266fd
Robert Lockay <robert.lockayhr@stu.austinisd.org>
2025-02-19 12:49:07 -0600

    added log in page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,26 +1,21 @@
 import { neon } from "@neondatabase/serverless";
 import styles from "./page.module.css"
-import Navbar from "../components/navbar/navbar";
+import Link from "next/link";
 
 const SignUp = () => {
-    const createUser = (formData: FormData) => {
-       
-    }
 
     return (
         <main className={styles.main}>
-            <Navbar />
             <form className={styles.form} >
-                <h1 className={styles.title}>Become a scavenger.</h1>
+                <h1 className={styles.title}>Ready to scavenge?</h1>
                 <div className={styles.inputs}>
                     <label className={styles.label}>Username</label>
                     <input className={styles.textInput} type="text" id="username" name="username"/>
                     <label className={styles.label}>Password</label>
                     <input className={styles.textInput} type="text" id="password" name="password"/>
-                    <label className={styles.label}>Contact Info</label>
-                    <input className={styles.textInput} type="text" id="contact" name="contact" placeholder="e.g. IG tag, phone number, email"/>
                 </div>
-                <button className={styles.submit} type="submit">Sign Up</button>
+                <button className={styles.submit} type="submit">Log In</button>
+                <Link href={"/signup"} className={styles.signup}>Don't have an account? Sign Up.</Link>
             </form>
         </main>
     )
