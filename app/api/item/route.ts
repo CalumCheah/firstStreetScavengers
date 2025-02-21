91c4b2bf6ebd4863f4fb1a6b55fd36e4f39827fb
Calum Cheah <calum.cheahue@stu.austinisd.org>
2025-02-21 09:57:08 -0600

    added api routes

---
+++ app/api/item/route.ts
@@ -0,0 +1,22 @@
+import { neon } from '@neondatabase/serverless';
+ 
+export async function GET() {
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const data = await sql('SELECT * FROM "first-street-scavengers"."Item";');
+ 
+    return Response.json({ data })
+}
+
+export async function POST(request: Request) {
+    const sql = neon(`${process.env.DATABASE_URL}`);
+    const req = await request.json()
+    const {title, description, imagePath} = req
+
+    try {
+        await sql(`INSERT INTO "first-street-scavengers"."Item" ("title", "description", "imagePath") VALUES ('${title}', '${description}', '${imagePath}')`)
+    } catch (e) {
+        return Response.json(e)
+    }
+
+    return Response.json(req)
+}
