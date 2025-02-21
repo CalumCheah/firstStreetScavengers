678f15027cd5303e99cf4c3e6d6b14e5b24826aa
Robert Lockay <robert.lockayhr@stu.austinisd.org>
2025-02-21 09:29:59 -0600

    quickfix done

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,17 +8,19 @@ const SignUp = () => {
     return (
         <main className={styles.main}>
             <Navbar />
-            <form className={styles.form} >
-                <h1 className={styles.title}>Ready to scavenge?</h1>
-                <div className={styles.inputs}>
-                    <label className={styles.label}>Username</label>
-                    <input className={styles.textInput} type="text" id="username" name="username"/>
-                    <label className={styles.label}>Password</label>
-                    <input className={styles.textInput} type="password" id="password" name="password"/>
-                </div>
-                <button className={styles.submit} type="submit">Log In</button>
-                <Link href={"/signup"} className={styles.signup}>Don't have an account? Sign Up.</Link>
-            </form>
+            <div className={styles.login}>
+                <form className={styles.form} >
+                    <h1 className={styles.title}>Ready to scavenge?</h1>
+                    <div className={styles.inputs}>
+                        <label className={styles.label}>Username</label>
+                        <input className={styles.textInput} type="text" id="username" name="username"/>
+                        <label className={styles.label}>Password</label>
+                        <input className={styles.textInput} type="password" id="password" name="password"/>
+                    </div>
+                    <button className={styles.submit} type="submit">Log In</button>
+                    <Link href={"/signup"} className={styles.signup}>Don't have an account? Sign Up.</Link>
+                </form>
+            </div>
         </main>
     )
 }
--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -10,18 +10,20 @@ const SignUp = () => {
     return (
         <main className={styles.main}>
             <Navbar />
-            <form className={styles.form} >
-                <h1 className={styles.title}>Become a scavenger.</h1>
-                <div className={styles.inputs}>
-                    <label className={styles.label}>Username</label>
-                    <input className={styles.textInput} type="text" id="username" name="username"/>
-                    <label className={styles.label}>Password</label>
-                    <input className={styles.textInput} type="password" id="password" name="password"/>
-                    <label className={styles.label}>Contact Info</label>
-                    <input className={styles.textInput} type="text" id="contact" name="contact" placeholder="e.g. IG tag, phone number, email"/>
-                </div>
-                <button className={styles.submit} type="submit">Sign Up</button>
-            </form>
+            <div className={styles.signup}>
+                <form className={styles.form} >
+                    <h1 className={styles.title}>Become a scavenger.</h1>
+                    <div className={styles.inputs}>
+                        <label className={styles.label}>Username</label>
+                        <input className={styles.textInput} type="text" id="username" name="username"/>
+                        <label className={styles.label}>Password</label>
+                        <input className={styles.textInput} type="password" id="password" name="password"/>
+                        <label className={styles.label}>Contact Info</label>
+                        <input className={styles.textInput} type="text" id="contact" name="contact" placeholder="e.g. IG tag, phone number, email"/>
+                    </div>
+                    <button className={styles.submit} type="submit">Sign Up</button>
+                </form>
+            </div>
         </main>
     )
 }
